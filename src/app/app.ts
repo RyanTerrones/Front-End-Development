517663936f0901c9bb93c04540de1d2ea6fab969
unknown <g00420671@atu.ie>
2026-02-25 11:35:49 +0000

    lab4

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,31 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListComponent } from './list-component/list-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('data-binding-app');
+
+  //varibles
+  count:number = 0;
+  hide:boolean=false;
+
+  //method for when img is double clicked, it will hide/show the img
+  onDoubleClicked(){
+    if(this.hide === true){
+      this.hide = false;
+    }
+    else{
+      this.hide = true;
+    }
+  }
+
+  //method to show a counter when button is clicked
+  onButtonClicked(){
+    this.count++;
+  }
 }
